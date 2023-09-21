c3b2d81eb0fca5ce7f2b4e1a0ab6139530993023
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2023-09-21 20:52:41 -0300

    config,routes,controller

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {danielF} from "./functions/functions.js";
+// import { danielF } from "./functions/functions.js";
+import {routes} from "./routes/routes.js";
 
-const func1 = danielF
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // app.use(express.static("public"));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-let newItems = []
-app.get("/", (req, res) => {
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var today  = new Date();
-    let day = today.toLocaleDateString("en-US", options)
-    res.render("index.ejs",{currentDay:day, newListItem: newItems});
-});
+// let newItems = [];
+// app.get("/", (req, res) => {
+//   let options = {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+//   };
+//   var today = new Date();
+//   let day = today.toLocaleDateString("en-US", options);
+//   res.render("index.ejs", { currentDay: day, newListItem: newItems });
+// });
+app.use(routes)
 
 app.get("/work", (req, res) => {
-    res.render("work.ejs")
+  res.render("work.ejs");
 });
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    newItems.push(item)
-    res.redirect("/");
-})
+  let item = req.body.newItem;
+  newItems.push(item);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});
